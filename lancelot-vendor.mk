03261d290a1c566668ba5bbe5006d3578850c519
TheMalachite <eliasgheeraert@gmail.com>
2021-02-14 10:44:02 +0100

    lancelot: Mention which interface is used for keymaster beanpod
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- lancelot-vendor.mk
+++ lancelot-vendor.mk
@@ -22,4 +22,5 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/lancelot/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
     vendor/xiaomi/lancelot/proprietary/bin/kpoc_charger:$(TARGET_COPY_OUT_SYSTEM)/bin/kpoc_charger \
     vendor/xiaomi/lancelot/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
-    vendor/xiaomi/lancelot/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so
+    vendor/xiaomi/lancelot/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
+    vendor/xiaomi/lancelot/proprietary/etc/init/android.hardware.keymaster@4.0-service.beanpod.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/android.hardware.keymaster@4.0-service.beanpod.rc
